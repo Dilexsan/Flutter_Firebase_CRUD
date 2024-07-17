4e4940d282df21e158c824d8b086507f025723bf
Dilexsan <120619547+Dilexsan@users.noreply.github.com>
2024-07-17 20:59:56 +0530

    adding command

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_connect/pages/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+// this is command
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
